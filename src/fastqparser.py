f322d2235ab2490b55191cf5a39b663d8657aeb6
Edward Mountjoy <emountjoy@gmail.com>
2014-12-03 22:44:19 +0000

    Added MIT license

--- src/fastqparser.py
+++ src/fastqparser.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Contains simple parsers for fasta and fastq files, taken directly from the
-# biopython source code.
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Edward Mountjoy
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 #
 
 import string
@@ -22,7 +41,10 @@ def phred_score_dict(offset):
     return phred_dict
 
 def fastqIterator(handle):
-    """Iterate over Fastq records 
+    """This function is adapted from the biopython source code. It therefore has
+       been distibuted under their own license:
+
+       ENTER LICENSE
     """
     # We need to call handle.readline() at least four times per record,
     # so we'll save a property look up each time:
