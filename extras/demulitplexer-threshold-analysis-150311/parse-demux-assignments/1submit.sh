b15385a501c7e4cea8b23d3ba526ec1d84ac1cf5
Edward Mountjoy <emountjoy@gmail.com>
2015-03-12 22:08:30 +0000

    a

---
+++ extras/demulitplexer-threshold-analysis-150311/parse-demux-assignments/1submit.sh
@@ -0,0 +1,5 @@
+indir=/data/home/em13383/other/aware-demultiplexer/extras/demulitplexer-threshold-analysis-150311/data-from-jack/M01520_93_000000000-A72MP
+
+for d in $(ls -d demult*); do
+    echo $d
+done
