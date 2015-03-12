7652ccd219730f55edffe6cb6a11f20d06fb64da
Ed Mountjoy <emountjoy@gmail.com>
2015-03-12 17:01:38 +0000

    c

--- extras/demulitplexer-threshold-analysis-150311/blast-multiplexed-seqs/3_blast-fasta.sh
+++ extras/demulitplexer-threshold-analysis-150311/blast-multiplexed-seqs/3_blast-fasta.sh
@@ -2,12 +2,10 @@ fastain=2out_raw-reads_chunck-*
 db=../blast-database/cell-line-db
 outpref=3out_blast-output
 
-echo "Starting blast processes..."
 for filen in $fastain; do
 	idx=${filen##*-}
 	idx=${idx%%.gz}
 	echo $idx
 	outname="$outpref"_chunck-"$idx".xml.gz
-	gzcat $filen | blastn -db $db -outfmt 5 -num_alignments 2 | gzip -c > $outname &
+	echo "gzcat $filen | blastn -db $db -outfmt 5 -num_alignments 2 | gzip -c > $outname "
 done
-echo "Started!"
