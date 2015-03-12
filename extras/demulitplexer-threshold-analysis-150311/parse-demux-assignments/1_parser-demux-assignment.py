ae67b6bf635394f87a2c4729de56430082b79508
Edward Mountjoy <emountjoy@gmail.com>
2015-03-12 22:04:56 +0000

    a

---
+++ extras/demulitplexer-threshold-analysis-150311/parse-demux-assignments/1_parser-demux-assignment.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+import sys
+import glob
+import gzip
+import os
+
+def main():
+
+    # Options
+    # demux_dir = "../data-from-jack/M01520_93_000000000-A72MP/demultiplexed-head/demultiplexed_prob0.05"
+    demux_dir = sys.argv[1]
+
+    # Get run name
+    runname = os.path.split(demux_dir)[1]
+    outname = "{0}_assingments.tsv".format(runname)
+
+    # Get list of cell line folders
+    cellline_folders = glob.glob(os.path.join(demux_dir, "*"))
+
+    with open(outname, "w") as out_h:
+        for cellline_path in cellline_folders:
+            cellline = os.path.split(cellline_path)[1]
+            # Get name of fastq
+            in_fastq = glob.glob(os.path.join(cellline_path,
+                                              "*.subset.fastq.gz"))[0]
+            # Parse fastq
+            with gzip.open(in_fastq, "r") as in_h:
+                for line in in_h:
+                    if line.startswith("@M"):
+                        readname = line.rstrip().lstrip("@")
+
+                        # Output readname and cellline
+                        lineout = [readname, cellline]
+                        out_h.write("\t".join(lineout) + "\n")
+                    
+
+    return 0
+   
+if __name__ == '__main__':
+    main()
+
+ 
