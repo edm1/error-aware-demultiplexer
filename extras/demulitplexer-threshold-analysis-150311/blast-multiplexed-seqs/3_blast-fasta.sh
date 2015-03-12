538fc33279901026b417183288f2d86f95907c6f
Ed Mountjoy <emountjoy@gmail.com>
2015-03-12 17:04:29 +0000

    c

--- extras/demulitplexer-threshold-analysis-150311/blast-multiplexed-seqs/3_blast-fasta.sh
+++ extras/demulitplexer-threshold-analysis-150311/blast-multiplexed-seqs/3_blast-fasta.sh
@@ -5,7 +5,6 @@ outpref=3out_blast-output
 for filen in $fastain; do
 	idx=${filen##*-}
 	idx=${idx%%.gz}
-	echo $idx
 	outname="$outpref"_chunck-"$idx".xml.gz
 	echo "gzcat $filen | blastn -db $db -outfmt 5 -num_alignments 2 | gzip -c > $outname "
-done
+done | parallel --pipe -j 4 bash
