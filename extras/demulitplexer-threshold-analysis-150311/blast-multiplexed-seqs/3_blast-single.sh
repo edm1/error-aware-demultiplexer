ccdc9acc389f9ea99f7977acbac154f6b0ba1c06
Ed Mountjoy <emountjoy@gmail.com>
2015-03-12 17:23:21 +0000

    c

--- extras/demulitplexer-threshold-analysis-150311/blast-multiplexed-seqs/3_blast-single.sh
+++ extras/demulitplexer-threshold-analysis-150311/blast-multiplexed-seqs/3_blast-single.sh
@@ -4,9 +4,9 @@ outpref=3out_blast-output
 
 # Derive
 fastain=$1
-idx=${filen##*-}
+idx=${fastain##*-}
 idx=${idx%%.gz}
 outname="$outpref"_chunck-"$idx".xml.gz
 
 # Run
-gzcat $filen | blastn -db $db -outfmt 5 -num_alignments 2 | gzip -c > $outname
+gzcat $fastain | blastn -db $db -outfmt 5 -num_alignments 2 | gzip -c > $outname
